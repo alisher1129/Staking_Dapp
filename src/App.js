00a99ae5722c324cfb24ea5decab680cd624c452
Ali Sher <ali.sher@vaival.com>
2024-03-08 15:14:51 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -9,22 +9,41 @@ import CheckStake from "./components/CheckStake";
 export default function Home() {
   return (
     <>
-      <div className=" mt-20  space-y-10">
-        <div className="flex justify-evenly">
-          <div className=" text-5xl font-bold">Staking Dapp</div>
-          <ConnectWallet />
-        </div>
-        <div className="flex flex-col  justify-center items-center space-y-10 ">
-          <CheckStake />
-          <BalanceOf />
-          <div className="flex ">
-            <StakeToken />
-            <UnStake />
-          </div>
+      <div className="flex justify-evenly mt-12">
+        <div className=" text-5xl font-bold">Staking Dapp</div>
+        <ConnectWallet />
+      </div>
 
-          <ClaimYourReward />
+
+
+
+
+
+
+
+
+<div className="flex justify-center">
+<div className="w-1/2 border-2 border-black border-solid  shadow rounded p-4 mt-10  ">
+        <div className="   space-y-10">
+          <div className="flex flex-col  justify-center items-center space-y-10 ">
+            <CheckStake />
+            <BalanceOf />
+            <div className="flex ">
+              <StakeToken />
+              <UnStake />
+            </div>
+
+            <ClaimYourReward />
+          </div>
         </div>
       </div>
+
+</div>
+
+
+
+
+    
     </>
   );
 }
